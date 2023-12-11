18e1d6d1c027c2d26a509867301a01e6100ee759
web-and-app <webandapp@confinedtechnologies.com>
2023-12-11 23:37:45 +0500

    .

--- src/components/userprofile/setting/ProfileVisibility.js
+++ src/components/userprofile/setting/ProfileVisibility.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ProfileVisibility = () => {
   return (
-    <div>ProfileVisibility</div>
+    <>
+      <p className="heading mt-3 clr-text">Profile Visibility Settings</p>
+      <p className="para text-dark">Select who may see your profile details.</p>
+
+      <div className="border-bottom mb-4"></div>
+
+     
+    </>
   )
 }
 
