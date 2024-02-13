488cdee20db6e7dcc8b41a3d5594f88b723af975
web-and-app <webandapp@confinedtechnologies.com>
2024-02-14 00:06:41 +0500

    .

--- src/components/userprofile/groups/GroupInv.js
+++ src/components/userprofile/groups/GroupInv.js
@@ -82,7 +82,7 @@ const GroupInv = ({ xl, md, lg }) => {
                                                 </div>
                                             </div>
                                             <Link className='link-hov' href={'#'}><p className="heading text-black mb-2 mt-4">admin</p></Link>
-                                            <p className="para clr-light">Active 2 minutes ago</p>
+                                            {/* <p className="para clr-light">Active 2 minutes ago</p> */}
                                             <div className="imgtoimg">
                                                 <Link href={'#'}>
                                                     <Image src={'/assets/images/Modal/Avatar.png'} alt="" width={100} height={100} className='post-profile-sm'></Image>
