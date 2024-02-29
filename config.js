623dc3c1d4fb80f7b550186d5b281da041794369
mhunainaslam <hunainaslam.ha@gmail.com>
2024-02-29 16:42:54 +0500

    .

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-module.exports = {
-    APP_URL: "https://post.designingstudiollc.com",
-    IMG_URL: "https://post.designingstudiollc.com/storage",
-}
 // module.exports = {
-//     APP_URL: "https://api.microndeveloper.com",
-//     IMG_URL: "https://api.microndeveloper.com/storage",
+//     APP_URL: "https://post.designingstudiollc.com",
+//     IMG_URL: "https://post.designingstudiollc.com/storage",
 // }
+module.exports = {
+    APP_URL: "https://api.microndeveloper.com",
+    IMG_URL: "https://api.microndeveloper.com/storage",
+}
